c31178651e384c6033407db0e86b85df7e8eca17
ymink716 <ymink716@gmail.com>
2023-08-03 06:39:19 +0900

    feat: 사용자 프로필 API 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,15 @@
-import { CreateUserDto } from './dto/create-user.dto';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from './users.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  // 회원 가입
-  @Post()
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    const user = await this.usersService.createUser(createUserDto);
-
-    return user;
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  getProfile(@Req() req: Request) {
+    return req.user;
   }
-
-  // 내 정보 조회
 }
