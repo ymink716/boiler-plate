3c0eb7769ee297a57944d6025d95fe739b226626
ymink716 <ymink716@gmail.com>
2023-08-03 04:35:48 +0900

    feat: google strategy 작성

---
+++ src/auth/strategy/google.strategy.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {}
