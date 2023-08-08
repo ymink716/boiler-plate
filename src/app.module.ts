1741c0779ba31077c3c12dc492d8d73b2f8ec0b7
ymink716 <ymink716@gmail.com>
2023-08-08 15:18:28 +0900

    feat: 불필요한 파일 파일 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { configModuleConfig } from './config/config-module.config';
@@ -12,8 +10,7 @@ import { typeormModuleConfig } from './config/typeorm-module.config';
     typeormModuleConfig,
     AuthModule,
     UsersModule,
-],
-  controllers: [AppController],
-  providers: [AppService],
+  ],
 })
+
 export class AppModule {}
