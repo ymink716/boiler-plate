afbdd589a84802fb257c8ac16b73d229fcd9abeb
ymink716 <ymink716@gmail.com>
2023-08-15 20:05:00 +0900

    feat: auth 모듈에서 user 모듈 주입 방식 변경

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,19 +3,18 @@ import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './strategy/google.strategy';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entity/user.entity';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { JwtRefreshStrategy } from './strategy/jwt-refresh.strategy';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     JwtModule.register({}),
-    TypeOrmModule.forFeature([User]),
     ConfigModule,
-
+    UsersModule,
   ],
   controllers: [AuthController],
   providers: [
@@ -23,7 +22,6 @@ import { ConfigModule } from '@nestjs/config';
     JwtStrategy,
     JwtRefreshStrategy,
     AuthService, 
-    UsersService, 
   ]
 })
 export class AuthModule {}
