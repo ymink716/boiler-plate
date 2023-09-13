28048659ee3ca2e96f18f4225d15cf0c10ddc2c4
ymink716 <ymink716@gmail.com>
2023-09-14 00:53:35 +0900

    test: QuestionsService 테스트 코드 작성

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,9 +1,9 @@
-import { Inject, Injectable, NotFoundException, ForbiddenException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, ForbiddenException, BadRequestException } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { QuestionsRepository } from './questions.repository';
 import { Question } from './entity/question.entity';
 import { User } from 'src/users/entity/user.entity';
-import { IsNotQuestionWriter, QuestionNotFound } from 'src/common/exception/error-types';
+import { InvalidQuestionContent, InvalidQuestionTitle, IsNotQuestionWriter, QuestionNotFound } from 'src/common/exception/error-types';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Injectable()
@@ -16,8 +16,9 @@ export class QuestionsService {
   async postQuestion(createQuestionDto: CreateQuestionDto, writer: User) {
     const { title, content } = createQuestionDto;
 
-    const question = new Question({ title, content, writer });
+    this.isValidQeustionDto(title, content);
 
+    const question = new Question({ title, content, writer });
     const newQuestion = this.questionsRepository.save(question);
 
     return newQuestion;
@@ -39,14 +40,15 @@ export class QuestionsService {
 
   async updateQuestion(questionId: number, user: User, updateQuestionDto: UpdateQuestionDto) {
     const question = await this.questionsRepository.findOneById(questionId);
-    
+
     if (!question) {
       throw new NotFoundException(QuestionNotFound.message, QuestionNotFound.name);
     }
 
-    this.isWriter(question, user);
-
     const { title, content } = updateQuestionDto;
+
+    this.isValidQeustionDto(title, content);
+    this.isWriter(question, user);
 
     const updatedQuestion = await this.questionsRepository.update(question, title, content);
 
@@ -65,11 +67,21 @@ export class QuestionsService {
     await this.questionsRepository.softDelete(questionId);
   }
 
-  isWriter(question: Question, writer: User) {
+  isWriter(question: Question, writer: User): void {
     const isWriter = (question.writer.id === writer.id);
     
     if (!isWriter) {
       throw new ForbiddenException(IsNotQuestionWriter.message, IsNotQuestionWriter.name);
+    }
+  }
+
+  isValidQeustionDto(title: string, content: string): void {
+    if (title.length < 2 || title.length > 50) {
+      throw new BadRequestException(InvalidQuestionTitle.message, InvalidQuestionTitle.name);
+    }
+
+    if (content.length < 2 || content.length > 500) {
+      throw new BadRequestException(InvalidQuestionContent.message, InvalidQuestionContent.name);
     }
   }
 }
--- src/questions/tests/questions.service.spec.ts
+++ src/questions/tests/questions.service.spec.ts
@@ -1,66 +1,234 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExecutionContext, INestApplication, NotFoundException, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
-import { DataSource } from "typeorm"
+import { BadRequestException, ForbiddenException, INestApplication, NotFoundException, ValidationPipe } from '@nestjs/common';
 import { Question } from 'src/questions/entity/question.entity';
 import { User } from 'src/users/entity/user.entity';
-import { UserProvider } from 'src/common/enums/user-provider.enum';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Bookmark } from 'src/bookmarks/entity/bookmark.entity';
 import { QuestionsService } from '../questions.service';
 import { AppModule } from 'src/app.module';
-import { QuestionsRepository } from '../questions.repository';
+import { TestQuestionsRepository } from './test-questions.repository';
+import { setUpTestingAppModule } from 'src/config/app-test.config';
+import { CreateQuestionDto } from '../dto/create-question.dto';
+import { UpdateQuestionDto } from '../dto/update-question.dto';
 
-describe('QuestionService', () => {
+describe('QuestionsService', () => {
   let app: INestApplication;
   let questionsService: QuestionsService;
-  let questionsRepository: QuestionsRepository;
+  let questionsRepository: TestQuestionsRepository;
+
+  let user: User;
+  let question: Question;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
+    .overrideProvider('QUESTIONS_REPOSITORY')
+    .useClass(TestQuestionsRepository)
     .compile();
 
     app = moduleFixture.createNestApplication();
     questionsService = app.get<QuestionsService>(QuestionsService);
-    questionsRepository = app.get<QuestionsRepository>('QUESTIONS_REPOSITORY');
+    questionsRepository = app.get<TestQuestionsRepository>('QUESTIONS_REPOSITORY');
 
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      })
-    );
+    setUpTestingAppModule(app);
     
     await app.init();
+
+    user = { id: 1 } as User;
+    question = new Question({
+      title: "test",
+      content: "test content...",
+      writer: user,
+    });
+  });
+
+  beforeEach(() => {
+    questionsRepository.reset();
+  });
+
+  describe('postQuestion()', () => {
+    test('question을 생성하고 생성된 question을 리턴한다.', async () => {
+      const createQuestionDto: CreateQuestionDto = {
+        title: 'test quesetion title',
+        content: 'test question content...',
+      };
+
+      const result = await questionsService.postQuestion(createQuestionDto, user);
+
+      expect(result).toBeInstanceOf(Question);
+      expect(result.title).toBe('test quesetion title');
+      expect(result.content).toBe('test question content...');
+    });
+
+    test.each([['t'], ['0'.repeat(51)]])(
+      '제목이 2글자 미만, 50글자를 초과하면 BadRequestException이 발생한다.',
+      async (title) => {
+        const createQuestionDto: CreateQuestionDto = {
+          title: title,
+          content: 'test question content...',
+        };
+        
+        await expect(
+          questionsService.postQuestion(createQuestionDto, user),
+        ).rejects.toThrow(BadRequestException);
+      },
+    );
+
+    test.each([['t'], ['0'.repeat(501)]])(
+      '내용이 2글자 미만, 500글자를 초과하면 BadRequestException이 발생한다.',
+      async (content) => {
+        const createQuestionDto: CreateQuestionDto = {
+          title: 'test',
+          content: content,
+        };
+        
+        await expect(
+          questionsService.postQuestion(createQuestionDto, user),
+        ).rejects.toThrow(BadRequestException);
+      },
+    );
+  });
+
+  describe('getQuestions()', () => {
+    test('question 목록을 조회한다.', async () => {
+      const question1 = new Question({
+        title: "test1",
+        content: "test content...",
+        writer: user,
+      });
+  
+      const question2 = new Question({
+        title: "test2",
+        content: "test content...",
+        writer: user,
+      });
+
+      questionsRepository.save(question1);
+      questionsRepository.save(question2);
+
+      const result = await questionsService.getQuestions();
+
+      expect(result.length).toBe(2);
+      expect(result).toEqual([question1, question2]);
+    });
   });
 
   describe('getQuestion()', () => {
-    test('DB에 question의 데이터가 있을 경우 성공적으로 question 데이터로 응답한다.', async () => {
-      // given
-      const question = {
-        id: 1,
-        title: "a",
-        content: "co"
-      } as Question;
-      
-      jest.spyOn(questionsRepository, 'findOneById').mockResolvedValue(question)
+    test('DB에 question의 데이터가 있다면, question 데이터를 리턴한다.', async () => {
+      questionsRepository.save(question);
 
-      // when
       const result = await questionsService.getQuestion(question.id);
 
-      // then
       expect(result).toEqual(question);
     });
 
-    test('DB에 question의 데이터가 있을 경우 성공적으로 question 데이터로 응답한다.', async () => {
-      // given
-      jest.spyOn(questionsRepository, 'findOneById').mockResolvedValue(null)
+    test('DB에 question의 데이터가 없다면, NotFoundException이 발생한다.', async () => {
+      await expect(
+        questionsService.getQuestion(0)
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
 
-      // when, then
-      await expect(questionsService.getQuestion(1)).rejects.toThrow(NotFoundException);
+  describe('updateQuestion()', () => {
+    const updateQuestionDto: UpdateQuestionDto = {
+      title: 'test quesetion title(수정)',
+      content: 'test question content...(수정)',
+    };
+
+    test('question을 수정하고, 수정된 question을 리턴한다.', async () => {
+      questionsRepository.save(question);
+
+      jest.spyOn(questionsService, 'isWriter').mockReturnValue(undefined);
+
+      const result = await questionsService.updateQuestion(
+        question.id, user, updateQuestionDto
+      );
+
+      expect(result).toBeInstanceOf(Question);
+      expect(result.title).toBe('test quesetion title(수정)');
+      expect(result.content).toBe('test question content...(수정)');
+    });
+
+    test('DB에 question의 데이터가 없다면, NotFoundException이 발생한다.', async () => {
+      await expect(
+        questionsService.updateQuestion(0, user, new UpdateQuestionDto())
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    test.each([['t'], ['0'.repeat(51)]])(
+      '제목이 2글자 미만, 50글자를 초과하면 BadRequestException이 발생한다.',
+      async (title) => {
+        questionsRepository.save(question);
+
+        const updateQuestionDto: UpdateQuestionDto = {
+          title: title,
+          content: 'test question content...',
+        };
+        
+        await expect(
+          questionsService.updateQuestion(question.id, user, updateQuestionDto),
+        ).rejects.toThrow(BadRequestException);
+      },
+    );
+
+    test.each([['t'], ['0'.repeat(501)]])(
+      '내용이 2글자 미만, 500글자를 초과하면 BadRequestException이 발생한다.',
+      async (content) => {
+        questionsRepository.save(question);
+
+        const updateQuestionDto: UpdateQuestionDto = {
+          title: 'test',
+          content: content,
+        };
+        
+        await expect(
+          questionsService.updateQuestion(question.id, user, updateQuestionDto),
+        ).rejects.toThrow(BadRequestException);
+      },
+    );
+
+    test('작성자가 아니라면 ForbiddenException이 발생한다.', async () => {
+      questionsRepository.save(question);
+      const user2 = { id: 2 } as User;
+
+      jest.spyOn(questionsService, 'isWriter').mockImplementation(() => {
+        throw new ForbiddenException();
+      });
+
+      await expect(
+        questionsService.updateQuestion(question.id, user2, updateQuestionDto),
+      ).rejects.toThrow(ForbiddenException);
+    });
+  });
+
+  describe('deleteQuestion()', () => {
+    test('해당 question을 삭제하고 리턴하지 않는다.', async () => {
+      const savedQuestion = await questionsRepository.save(question);
+      jest.spyOn(questionsService, 'isWriter').mockReturnValue(undefined);
+
+      const result = await questionsService.deleteQuestion(
+        savedQuestion.id, user
+      );
+
+      expect(result).toBeUndefined();
+    });
+
+    test('DB에 question의 데이터가 없다면, NotFoundException이 발생한다.', async () => {
+      await expect(
+        questionsService.deleteQuestion(0, user),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    test('작성자가 아니라면 ForbiddenException이 발생한다.', async () => {
+      const savedQuestion = await questionsRepository.save(question);
+      const user2 = { id: 2 } as User;
+
+      jest.spyOn(questionsService, 'isWriter').mockImplementation(() => {
+        throw new ForbiddenException();
+      });
+
+      await expect(
+        questionsService.deleteQuestion(savedQuestion.id, user2),
+      ).rejects.toThrow(ForbiddenException);
     });
   });
 
