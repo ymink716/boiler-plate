0eea030b94dc0b2306483406b8007c4a0edfcf19
ymink716 <ymink716@gmail.com>
2023-10-26 01:32:57 +0900

    feat: health check api에 인스턴스 정보 추가

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import * as ip from 'ip';
 
 @Controller('health')
 export class HealthCheckController {
   @Get('/')
   healthCheck() {
-    return 'health check';
+    return `health check, ${ip.address()}`;
   }
 }
